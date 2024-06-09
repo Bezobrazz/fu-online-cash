b62d6c5e0cbe29a14968c4f4537eb96be307a20e
Viacheslav Kostenko <kostenko.uvd@gmail.com>
2024-06-09 12:36:11 +0300

    added custom sidebar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,12 @@
 import { GiHamburgerMenu } from "react-icons/gi";
-export const Header = () => {
+export const Header = ({ openSidebar }: { openSidebar: () => void }) => {
   return (
     <header className="relative container p-[20px] flex justify-center items-center bg-teal-500">
       <GiHamburgerMenu
         size={30}
         fill="white"
-        className="absolute top-[50%] left-10 translate-y-[-50%] lg:hidden"
+        className="absolute top-[50%] left-10 translate-y-[-50%] lg:hidden cursor-pointer"
+        onClick={openSidebar}
       />
       <h2 className="text-white text-[20px]">Продаж товарів</h2>
     </header>
