1983b397dfbeee27cb2999572428bd248aeafd7f
Bezobrazz <kostenko.uvd@gmail.com>
2024-06-15 18:54:34 +0300

    added aside for rpoduct categories

--- src/pages/ProductsServices.tsx
+++ src/pages/ProductsServices.tsx
@@ -1,8 +1,18 @@
+import { FaRegFolder } from "react-icons/fa6";
 
 
  const ProductsServices = () => {
+
+  const categories = ["Агроволокно", "Садовий бордюр", "Кора соснова", "Торфи"]
   return (
-    <div>Products Services</div>
+    <div className="h-screen">
+      <ul className="w-28 bg-slate-200 h-full p-2 flex flex-col gap-4 ">
+        {categories.map((categorie, index) => (<li key={index} className="flex flex-col items-center justify-items-center">
+          <div className="w-20 h-20 rounded-md bg-white flex items-center justify-center"><FaRegFolder/></div>
+          <p>{categorie}</p>
+        </li>))}
+      </ul>
+    </div>
   )
 }
 export default ProductsServices
