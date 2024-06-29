6499788dc9a742e01d0ec87ee069dec1edd2e877
Bezobrazz <kostenko.uvd@gmail.com>
2024-06-29 22:31:28 +0300

    separated categories bar to different component

---
+++ src/components/CategoriesBar/CategoriesBar.tsx
@@ -0,0 +1,17 @@
+import { FaRegFolder } from "react-icons/fa6";
+
+export const CategoriesBar = () => {
+    const categories = ["Агроволокно", "Садовий бордюр", "Кора соснова", "Торфи"]
+  return (
+    <div className="h-screen">
+      <ul className="w-28 bg-slate-200 h-full p-2 flex flex-col gap-4 ">
+        {categories.map((categorie, index) => (<li key={index} className="flex flex-col items-center justify-items-start">
+          <div className="w-20 h-20 rounded-md bg-white flex items-center justify-center"><FaRegFolder/>
+          </div>
+           <p>{categorie}</p>
+         
+        </li>))}
+      </ul>
+    </div>
+  )
+}
