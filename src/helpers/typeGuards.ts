b418d9aadd37d55abba37b0e3bacf2ca8b5a9b0e
VasylPopaz <pafos1997@gmail.com>
2024-08-05 23:51:52 +0200

    add helper typeGuards

---
+++ src/helpers/typeGuards.ts
@@ -0,0 +1,26 @@
+import {
+  Product,
+  SalePoint,
+  type CardListItemType,
+  type Cashbox,
+  type UserInfo,
+} from "../types";
+
+export const isUserInfo = (item: CardListItemType): item is UserInfo => {
+  return (item as UserInfo).role !== undefined;
+};
+
+export const isCashbox = (item: CardListItemType): item is Cashbox => {
+  return (item as Cashbox).card !== undefined;
+};
+
+export const isProduct = (item: CardListItemType): item is Product => {
+  return (item as Product).article !== undefined;
+};
+
+export const isSalePoint = (item: CardListItemType): item is SalePoint => {
+  return (
+    (item as SalePoint).enterpriseId !== undefined &&
+    (item as SalePoint).title !== undefined
+  );
+};
