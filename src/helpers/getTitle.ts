bfbbbd374e5ca4acf98a94bd47d5acbd835c480a
VasylPopaz <pafos1997@gmail.com>
2024-08-05 23:58:44 +0200

    add helper getTitle

---
+++ src/helpers/getTitle.ts
@@ -0,0 +1,10 @@
+import { CardListItemType } from "../types";
+import { isCashbox, isProduct, isSalePoint, isUserInfo } from "./typeGuards";
+
+export const getTitle = (item: CardListItemType) => {
+  if (isUserInfo(item)) return "Редагування працівника";
+  if (isSalePoint(item)) return "Редагування торгової точки";
+  if (isCashbox(item)) return "Редагування каси";
+  if (isProduct(item)) return "Редагування продукту";
+  return "";
+};
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,4 @@
 export * from "./calculateTotalPrice";
 export * from "./isTitleUnique";
 export * from "./typeGuards";
+export * from "./getTitle";
