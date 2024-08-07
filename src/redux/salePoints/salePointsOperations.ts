a93ab02b1e8cc158223e4defa3ef007cd5bfc937
VasylPopaz <pafos1997@gmail.com>
2024-08-07 15:14:38 +0200

    add addSalePoint Thunk

--- src/redux/salePoints/salePointsOperations.ts
+++ src/redux/salePoints/salePointsOperations.ts
@@ -1,7 +1,12 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-import type { SalePoint } from "../../types";
-import { getCollectionData } from "../../firebase";
+import type { NewSalePoint, SalePoint } from "../../types";
+import {
+  addDocumentToCollection,
+  deleteDocumentById,
+  editDocumentById,
+  getCollectionData,
+} from "../../firebase";
 
 export const getSalePoints = createAsyncThunk<
   SalePoint[],
@@ -11,6 +16,23 @@ export const getSalePoints = createAsyncThunk<
   try {
     const data = await getCollectionData<SalePoint>("salePoints");
     return data;
+  } catch (error) {
+    if (error instanceof Error) {
+      return rejectWithValue(error.message);
+    } else {
+      return rejectWithValue("An unknown error occurred");
+    }
+  }
+});
+
+export const addSalePoint = createAsyncThunk<
+  SalePoint,
+  NewSalePoint,
+  { rejectValue: string }
+>("salePoints/addSalePoint", async (salePoint, { rejectWithValue }) => {
+  try {
+    const newSalePoint = await addDocumentToCollection("salePoints", salePoint);
+    return newSalePoint;
   } catch (error) {
     if (error instanceof Error) {
       return rejectWithValue(error.message);
