f263a562ce28ccf02d75ccd6bcfaa0eb7724dddf
VasylPopaz <pafos1997@gmail.com>
2024-08-07 15:17:50 +0200

    upd SalePointForm for create and update salePoint

--- src/pages/SalePoints.tsx
+++ src/pages/SalePoints.tsx
@@ -1,20 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 import { Button, CardList, SalePointForm, Modal } from "../components";
 
-import { getSalePoints } from "../firebase";
-import { useModal } from "../hooks";
+import { getSalePoints, selectSalePoints } from "../redux";
+import { useAppDispatch, useAppSelector, useModal } from "../hooks";
 
 const SalePoints = () => {
-  const [salePoints, setSalePoints] = useState([]);
+  const dispatch = useAppDispatch();
+
+  const salePoints = useAppSelector(selectSalePoints);
 
   const [isOpenModal, toggleModal] = useModal();
 
   useEffect(() => {
-    getSalePoints().then((res) => {
-      setSalePoints(res);
-    });
-  }, []);
+    dispatch(getSalePoints());
+  }, [dispatch]);
   return (
     <section className="p-5 space-y-4">
       <CardList title="Список торгових точок" items={salePoints} />
