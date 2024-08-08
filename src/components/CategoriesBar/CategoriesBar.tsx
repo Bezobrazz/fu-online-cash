b872c582eb389249a972259a88727caba8da6d72
TopdevVitaliy <kostenko.uvd@gmail.com>
2024-08-08 16:56:20 +0300

    fetched product categories and render it from bd

--- src/components/CategoriesBar/CategoriesBar.tsx
+++ src/components/CategoriesBar/CategoriesBar.tsx
@@ -1,17 +1,27 @@
 import { FaRegFolder } from "react-icons/fa6";
+import { useSelector } from "react-redux";
+import { getCategories, selectCategories } from "../../redux";
+import { useEffect } from "react";
+import { useAppDispatch } from "../../hooks";
 
 export const CategoriesBar = () => {
-  const categories = ["Агроволокно", "Садовий бордюр", "Кора соснова", "Торфи"];
+  const categories = useSelector(selectCategories);
+  console.log(categories);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getCategories());
+  }, [dispatch]);
 
   return (
     <div>
       <ul className="w-28 h-full lg:h-full lg:w-full bg-slate-200 p-2 flex flex-col lg:flex-row gap-4 items-center">
-        {categories.map((categorie, index) => (
-          <li key={index} className="flex flex-col w-[90px]">
+        {categories.map((categorie) => (
+          <li key={categorie.id} className="flex flex-col w-[90px]">
             <div className="w-20 h-20 rounded-md bg-white flex items-center justify-center self-center">
               <FaRegFolder />
             </div>
-            <p className="truncate">{categorie}</p>
+            <p className="truncate">{categorie.title}</p>
           </li>
         ))}
       </ul>
